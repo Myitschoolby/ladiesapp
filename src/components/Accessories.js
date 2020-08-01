e30fd4f87d968ee33a3d83bae0419d4fdd35ba03
Kirill Bogdanov <kupuk.87@gmail.com>
2020-08-01 15:07:57 +0300

    add

--- src/components/Accessories.js
+++ src/components/Accessories.js
@@ -3,7 +3,46 @@ import React, { Component } from 'react';
 class Accessories extends Component {
     render() {
         return (
-            <div className="catalog_accessories">Accessories</div>
+            <div className="catalog_accessories">
+                <div className="catalog_accessories_header">
+                        Accessories
+                </div>
+                <div className="catalog_accessories_list">
+                    <ul>
+                        <li className="catalog_accessory_block">
+                            <div className="catalog_accessory">
+                                <div className="catalog_accessory_price">$19,00</div>
+                                <div className="catalog_accessory_thumbnail"><img src="/images/catalog/acc1.png" alt="#" /></div>
+                                <div className="catalog_accessory_description">
+                                    <div className="catalog_accessory_name">Comb 2.0</div>
+                                    <div className="catalog_accessory_caption">Original Version</div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="catalog_accessory_block">
+                            <div className="catalog_accessory">
+                                <div className="catalog_accessory_price">$19,00</div>
+                                <div className="catalog_accessory_thumbnail"><img src="/images/catalog/acc2.png" alt="#" /></div>
+                                <div className="catalog_accessory_description">
+                                    <div className="catalog_accessory_name">Watch+</div>
+                                    <div className="catalog_accessory_caption">Trending 2020</div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="catalog_accessory_block">
+                            <div className="catalog_accessory">
+                                <div className="catalog_accessory_price">$19,00</div>
+                                <div className="catalog_accessory_thumbnail"><img src="/images/catalog/acc3.png" alt="#" /></div>
+                                <div className="catalog_accessory_description">
+                                    <div className="catalog_accessory_name">Camera	&#174;</div>
+                                    <div className="catalog_accessory_caption">Classic 1992</div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
         );
     }
 }
