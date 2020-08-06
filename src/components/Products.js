3d2fcb3d39002c64139261f014b90679d9e57cba
Kirill Bogdanov <44680936+KuPuK87@users.noreply.github.com>
2020-08-06 19:44:03 +0300

    update

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,6 +1,113 @@
 import React, { Component } from 'react';
 
+import ProductsData from '../data/productsData.json';
+
 class Products extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            count: ProductsData.length,
+            page: 1,
+            list: []
+        };
+
+        this.prevPage = this.prevPage.bind(this);
+        this.nextPage = this.nextPage.bind(this);
+    }
+
+    getProducts(page) {
+        let count = 0;
+
+        const productsList = ProductsData.filter(function (product, index) {
+            count++;
+            return count <= page * 5;
+        });
+
+        let list = productsList.map(function (product, index) {
+            return (
+                <li key={index} className="catalog_product">
+                    <div className="catalog_product_thumbnail"><img src={product.thumbnail} alt="#" /></div>
+                    <div className="catalog_product_description">
+                        <div className="catalog_product_name">{product.name}</div>
+                        <div className="catalog_product_caption">{product.caption}</div>
+                        <div className="catalog_product_price">{product.price}</div>
+                        <button className="button button_cart"><span></span></button>
+                    </div>
+                </li>);
+        });
+
+        this.setState({
+            page: page,
+            list: list
+        });
+    }
+
+    prevPage(event) {
+        // let prevPage = this.state.page - 1;
+
+        // if (prevPage >= 1) this.getProducts(prevPage);
+
+        const parent = event.target.closest('.catalog_products');
+
+        if (!parent) return;
+
+        const list = parent.querySelector('.catalog_products_list > ul'),
+            firstItem = list.querySelector('li:first-child');
+
+        if (!list || !firstItem) return;
+
+        let parentWidth = list.clientWidth;
+        let firstItemStyles = window.getComputedStyle(firstItem);
+        let firstItemWidth = firstItem.clientWidth;
+
+        if (firstItemStyles.getPropertyValue('margin-right')) firstItemWidth += parseInt(firstItemStyles.getPropertyValue('margin-right'));
+
+        let currentFirstItemMl = parseInt(firstItem.style.marginLeft);
+
+        if (currentFirstItemMl < 0) firstItem.style.marginLeft = (currentFirstItemMl + firstItemWidth + 'px')    
+    }
+
+    nextPage(event) {
+        // let nextPage = this.state.page + 1;
+
+        // if (nextPage*5 <= this.state.count) this.getProducts(nextPage);
+
+        const parent = event.target.closest('.catalog_products');
+
+        if (!parent) return;
+
+        const list = parent.querySelector('.catalog_products_list > ul'),
+            firstItem = list.querySelector('li:first-child'),
+            lastItem = list.querySelector('li:first-child');
+
+        if (!list || !firstItem || !lastItem) return;
+
+        let parentWidth = list.clientWidth;
+        let firstItemStyles = window.getComputedStyle(firstItem);
+        let firstItemWidth = firstItem.clientWidth;
+        let lastItemWidth = lastItem.clientWidth;
+
+        if (firstItemStyles.getPropertyValue('margin-right')) firstItemWidth += parseInt(firstItemStyles.getPropertyValue('margin-right'));
+
+        let currentFirstItemMl = Math.abs(parseInt(firstItem.style.marginLeft));
+
+        if (currentFirstItemMl > 0) firstItemWidth += currentFirstItemMl;
+
+        console.log(parentWidth)
+        console.log(firstItemWidth)
+        console.log(lastItemWidth)
+        console.log(currentFirstItemMl)
+
+        firstItem.style.marginLeft = `-${firstItemWidth}px`;
+
+        
+    }
+
+    componentDidMount() {
+        this.getProducts(this.state.page);
+    }
+
     render() {
         return (
             <div className="catalog_products">
@@ -14,48 +121,13 @@ class Products extends Component {
                         </div>
                     </div>
                     <div className="catalog_products_navigation">
-                        <button className="button button_navigation_next"><i className="ico_arrow ico_arrow-left"></i></button>
-                        <button className="button button_navigation_prev"><i className="ico_arrow ico_arrow-right"></i></button>
+                        <button onClick={this.prevPage} className="button button_navigation_prev"><i className="ico_arrow ico_arrow-left"></i></button>
+                        <button onClick={this.nextPage} className="button button_navigation_next"><i className="ico_arrow ico_arrow-right"></i></button>
                     </div>
                 </div>
                 <div className="catalog_products_list">
                     <ul>
-                        <li className="catalog_product">
-                            <div className="catalog_product_thumbnail"><img src="/images/catalog/pr1.png" alt="#" /></div>
-                            <div className="catalog_product_description">
-                                <div className="catalog_product_name">Face Mask</div>
-                                <div className="catalog_product_caption">Grape Fruit</div>
-                                <div className="catalog_product_price">$16,00</div>
-                                <button className="button button_cart"><span></span></button>
-                            </div>
-                        </li>
-                        <li className="catalog_product">
-                            <div className="catalog_product_thumbnail"><img src="/images/catalog/pr1.png" alt="#" /></div>
-                            <div className="catalog_product_description">
-                                <div className="catalog_product_name">Face Mask</div>
-                                <div className="catalog_product_caption">Grape Fruit</div>
-                                <div className="catalog_product_price">$16,00</div>
-                                <button className="button button_cart"><span></span></button>
-                            </div>
-                        </li>
-                        <li className="catalog_product">
-                            <div className="catalog_product_thumbnail"><img src="/images/catalog/pr1.png" alt="#" /></div>
-                            <div className="catalog_product_description">
-                                <div className="catalog_product_name">Face Mask</div>
-                                <div className="catalog_product_caption">Grape Fruit</div>
-                                <div className="catalog_product_price">$16,00</div>
-                                <button className="button button_cart"><span></span></button>
-                            </div>
-                        </li>
-                        <li className="catalog_product">
-                            <div className="catalog_product_thumbnail"><img src="/images/catalog/pr1.png" alt="#" /></div>
-                            <div className="catalog_product_description">
-                                <div className="catalog_product_name">Face Mask</div>
-                                <div className="catalog_product_caption">Grape Fruit</div>
-                                <div className="catalog_product_price">$16,00</div>
-                                <button className="button button_cart"><span></span></button>
-                            </div>
-                        </li>
+                        {this.state.list}
                     </ul>
                 </div>
             </div>
